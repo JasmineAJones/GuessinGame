1cdc77db2c65ab85a142c73a1b0d1199226b55d9
JasmineAJones <94132788+JasmineAJones@users.noreply.github.com>
2022-06-08 11:32:19 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
-from cmath import sqrt
-from datetime import datetime
-from random import random, uniform
-from xml.sax.handler import feature_external_ges
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAbstractScrollArea
-from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.units import inch
-import openpyxl
-import os
-from reportlab.lib.pagesizes import letter
-import textwrap
-from reportlab.lib import colors
-from datetime import datetime
 import random
 
+##Learn more about hashmaps
 
-#print(int(uniform(1,4)))
-p = "$35%.90"
-speChars = '%"|$'
-pp = p.replace(speChars, '')
-print(pp)
+print("Options: ")
+hashbrowns={1:['green','blue'], 2:['white','yellow'], 3:['pink','purple']}
+
+for number in hashbrowns:
+    for i in range(2):
+        print(hashbrowns[number][i])
+    
+print("--------------")
+z = input("Please guess a color from list above: ")
+
+
+x = hashbrowns[int(random.uniform(1,4))][int(random.uniform(0,2))]
+y = hashbrowns[int(random.uniform(1,4))][int(random.uniform(0,2))]
+
+
+if x == y:
+    if x == z:
+        print("Correct! The answer was "+x)
+    else:
+        print("Wrong...the answer was "+x)
+
+else:
+    if x == z or y == z:
+        print("Correct! The answer was "+x+" or "+y)
+    else:
+        print("Wrong...the answer was "+x+" or "+y)
+
